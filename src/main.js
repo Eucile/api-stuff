66048cd6494ea50e4ae17b7d4eea0961e09a3eba
Chris Gosser and Krystal Foster <chris+krystal@>
2018-10-10 16:34:59 -0700

    add goodreads api

--- src/main.js
+++ src/main.js
@@ -3,27 +3,39 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(document).ready(function() {
 
 
-  $('#genre-btn').click(function() {
-    const searches = ["author/william%20gibson", "title/schismatrix", "title/snow%20crash", "title/synners", "title/shockwave%20rider"]
-    var search = searches[math.floor(math.random() * searches.length)];
-    $('#genre').text('');
-    var url = "https://goodquotesapi.herokuapp.com/${search}";
-    $('.genre-output').show();
-    $.ajax({
-      url: url,
-      success: function(result){
-      var finalIndex = result.data.length-1;
-      var randomIndex = Math.round((Math.random() * finalIndex));
-      console.log(randomIndex);
-      $('.genre-output h1').text(result.data[randomIndex].attributes.titles.en_jp);
-      $('#poster').attr("src", result.data[randomIndex].attributes.posterImage.medium);
-      $('.synopsis').text(result.data[randomIndex].attributes.synopsis);
+
+function quoter(){
+  const searches = ["author/william%20gibson", "author/bruce+sterling", "title/snow+crash", "title/shockwave+rider", "title/transmetropolitan", "author/richard+k.+morgan", "author/philip+k.+dick"];
+  var search = searches[Math.floor(Math.random() * searches.length)];
+  $('#genre').text('');
+  var url = "https://goodquotesapi.herokuapp.com/" + search;
+  $('.genre-output').show();
+  $.ajax({
+    url: url,
+    success: function(result){
+      var glorft = result.quotes[Math.floor(Math.random() * result.quotes.length)];
+      $('.genre-output h1').text(glorft.quote);
+      $('.author').text(glorft.author);
+      // $('#poster').attr("src", result.data[randomIndex].attributes.posterImage.medium);
+      // $('.synopsis').text(result.data[randomIndex].attributes.synopsis);
     }
-});
-console.log(url);
-});
+  });
+}
+function randint(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+function rando() {
+  console.log("rando is running");
+  var randyrandom = randint(10000, 60000)
+  console.log(randyrandom);
+  setTimeout(function(){quoter(); rando()}, randyrandom)
+}
+
+$(document).ready(function() {
+  quoter()
+  rando()
+
 
 });
